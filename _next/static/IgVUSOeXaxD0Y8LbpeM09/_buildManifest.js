f7037baeae8c465025dc897376555cf6b1e07722
unknown <rinstall@pc33.mcs.com>
2022-03-07 14:47:14 +0530

    Updates

---
+++ _next/static/IgVUSOeXaxD0Y8LbpeM09/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-e91cf33ccb1e4ddcdf6b.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
